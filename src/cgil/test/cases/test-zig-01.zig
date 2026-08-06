592512a42d82a508cb04e826b4fccc39640e7ac6
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-08-05 21:18:37 -0700

    Refactor Zig code generation and add test cases

---
+++ src/cgil/test/cases/test-zig-01.zig
@@ -0,0 +1,40 @@
+{
+	_sw0: switch ( @as( u32, 0 ) ) {
+		0 => {
+			continue :_sw0 1;
+		},
+		1 => {
+			{
+			
+			}
+			continue :_sw0 2;
+		},
+		2 => {
+			{
+			
+			}
+		},
+		else => unreachable,
+	}
+	
+}
+{
+	_sw1: switch ( @as( u32, 0 ) ) {
+		0 => {
+			continue :_sw1 1;
+		},
+		1 => {
+			{
+			
+			}
+			continue :_sw1 2;
+		},
+		2 => {
+			{
+			
+			}
+		},
+		else => unreachable,
+	}
+	
+}
